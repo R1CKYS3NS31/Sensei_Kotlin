5a67d00f4bf4e023c002c54af68eb33e1b01695a
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-11-01 11:54:03 +0300

    data class hashCode and copy function

--- src/main/kotlin/_01_introduction/helloworld.kt
+++ src/main/kotlin/_01_introduction/helloworld.kt
@@ -1,13 +1,15 @@
+package _01_introduction
+
 import java.io.File
 
 fun main(){
     println("hello world")
-    val files=File("test.txt").listFiles()
+    val files=File("./").listFiles()
     println(files?.size)//if not null shorthand
     println(files?.size?:"empty")//if not null else shorthand
     var a=1
     var b=3
-    println("before swap $a and $b")
+    println("before swap: $a and $b")
     a=b.also { b=a }
-    println("after swap $a and $b")
+    println("after swap: $a and $b")
 }
