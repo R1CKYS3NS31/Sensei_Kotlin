e3b110c9d7a2e36f5864d9fffb6278dfb635b720
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-12-13 14:11:45 +0300

    3_0_special_classes

--- src/main/kotlin/R_3_04_object_declaration.kt
+++ src/main/kotlin/R_3_04_object_declaration.kt
@@ -1,4 +1,7 @@
 object DoAuth{
+//     it will be created once when the object is accessed. Otherwise, it won't even be created.
+//     in Java this is Singleton pattern:
+//     it ensures you that only one instance of that class is created even if 2 threads try to create it.
     fun takeParams(userName:String,password:String){
         println("Input Auth parameters = $userName:$password")
     }
--- src/main/kotlin/R_3_04_object_expression.kt
+++ src/main/kotlin/R_3_04_object_expression.kt
@@ -1,5 +1,6 @@
 fun rentPrice(standardDays: Int, festivityDays: Int, specialDays: Int): Unit {
     val daysRates = object {
+//        Objects like this are often created in Java as anonymous class instances.
         var standard: Int = 30 * standardDays
         var festivity: Int = 50 * festivityDays
         var special: Int = 100 * specialDays
@@ -7,6 +8,7 @@ fun rentPrice(standardDays: Int, festivityDays: Int, specialDays: Int): Unit {
     val total = daysRates.standard + daysRates.festivity + daysRates.special
     print("Total Price: $$total")
 }
-fun  main(){
-    rentPrice(10,2,1)
+
+fun main() {
+    rentPrice(10, 2, 1)
 }
