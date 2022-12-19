5683e18f93335b1062a5e03d0668b64cb1d53f67
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-12-19 22:23:55 +0300

    8_0_productivity

---
+++ src/main/kotlin/R_8_04_smart_casts.kt
@@ -0,0 +1,21 @@
+import java.time.LocalDate
+import java.time.chrono.ChronoLocalDate
+
+fun main() {
+    val date: ChronoLocalDate? = LocalDate.now()
+    println(LocalDate.now())
+    println(date)
+    if (date != null) {
+        println(date.isLeapYear)
+    }
+    if (date != null && date.isLeapYear) {
+        println("$date is a leap year!")
+    }
+    if (date == null || !date.isLeapYear) {
+        println("There's is no Feb 29 this year...")
+    }
+    if (date is LocalDate) {
+        val month = date.monthValue
+        println(month)
+    }
+}
