0c485ae4285de5abbf6ef59227df68e75fb03750
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-12-19 12:24:41 +0300

    5_0_collections

---
+++ src/main/kotlin/R_5_15_map_element_access.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val map = mapOf("key" to 42)
+    val value1 = map["key"]
+    val value2 = map["key2"]
+    val value3 = map.getValue("key")
+    val mapWithDefault = map.withDefault { k ->
+        k.length
+    }
+    val value4 = mapWithDefault.getValue("key2")
+    val keys = map.keys
+    try {
+        map.getValue("anotherkey")
+    } catch (e: NoSuchElementException) {
+        println("Message: $e")
+    }
+    println("map: $map")
+    println("Value1 is $value1")
+    println("Value2 is $value2")
+    println("Value3 is $value3")
+    println("Value4 is $value4")
+    println("keys: $keys")
+}
