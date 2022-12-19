7709b5d9470031265678d3413bbe0c9b9b1f5481
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-12-19 12:43:14 +0300

    5_0_collections

--- src/main/kotlin/R_5_17_getOrElse.kt
+++ src/main/kotlin/R_5_17_getOrElse.kt
@@ -1,7 +1,15 @@
 fun main(){
     val list = listOf(0,10,20)
+    val map = mutableMapOf<String,Int?>()
+
+
     println(list.getOrElse(1){42}) // if there return the value
     println(list.getOrNull(10)) // if missing return null
     println(list.getOrElse(10){42}) // if missing return 42
-
+    println("-------------------------------")
+    println(map.getOrElse("x"){1})
+    map["x"] = 3
+    println(map.getOrElse("x"){2})
+    map["x"] = null
+    println(map.getOrElse("x"){3}) // if null return 3
 }
