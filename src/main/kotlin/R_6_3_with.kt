bf32c67e014b540ebfb656a83bca72be550e1abb
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-12-19 14:08:21 +0300

    6_0_scope_functions

---
+++ src/main/kotlin/R_6_3_with.kt
@@ -0,0 +1,11 @@
+class  Configuration(var host:String, var port:Int)
+fun main(){
+    val configuration = Configuration(host="127.0.0.1", port = 9000)
+//    with is a non-extension function that can access members of its argument concisely:
+//    you can omit the instance name when referring to its members.
+    with(configuration){
+        println("$host:$port")
+    }
+//    instead of
+    println("${configuration.host}:${configuration.port}")
+}
