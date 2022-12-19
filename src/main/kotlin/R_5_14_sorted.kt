01a6f670a2b7d8796b211c9e612d192f79c71442
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-12-19 12:10:10 +0300

    5_0_collections

---
+++ src/main/kotlin/R_5_14_sorted.kt
@@ -0,0 +1,15 @@
+import kotlin.math.abs
+
+fun main() {
+    val shuffled = listOf(5, 4, 2, 1, 3, -10)
+    val natural = shuffled.sorted()
+    val inverted = shuffled.sortedBy { -it }
+    val descending = shuffled.sortedDescending()
+    val descendingBy = shuffled.sortedByDescending { abs(it) }
+
+    println("Shuffled: $shuffled")
+    println("Natural order: $natural")
+    println("Inverted natural order: $inverted")
+    println("Inverted natural order value: $descending")
+    println("Inverted natural order of absolute values: $descendingBy")
+}
