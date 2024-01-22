eb1c6ae2041652211ec08593d86c8cf489da7165
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-01-22 11:46:21 +0300

    set

--- src/main/kotlin/R_5_02_set.kt
+++ src/main/kotlin/R_5_02_set.kt
@@ -1,4 +1,5 @@
 val openIssues: MutableSet<String> = mutableSetOf("uniqueDescr1", "uniqueDescr2", "uniqueDescr3")
+val readOnlyOpenIssues :Set<String> = openIssues
 
 // A set is an unordered collection that does not support duplicates.
 fun addIssue(uniqueDesc: String): Boolean {
@@ -8,10 +9,18 @@ fun getStatusLog(isAdded: Boolean): String {
     return if (isAdded) "registered correctly." else "marked as duplicate and rejected."
 }
 fun main() {
-    val aNewIssue: String = "uniqueDescr4"
-    val anIssueAlreadyIn: String = "uniqueDescr2"
+    val aNewIssue = "uniqueDescr4"
+    val anIssueAlreadyIn = "uniqueDescr2"
+
+//    readOnlyOpenIssues.add() // error
 
     println("Issue $aNewIssue ${getStatusLog(addIssue(aNewIssue))}")
     println("Issie $anIssueAlreadyIn ${getStatusLog((addIssue(anIssueAlreadyIn)))}")
     println("The set: ${openIssues.joinToString("...")}")
+
+    val readOnlyFruits = setOf("apple","banana","cherry","cherry")
+    println(readOnlyFruits)
+
+
+
 }
