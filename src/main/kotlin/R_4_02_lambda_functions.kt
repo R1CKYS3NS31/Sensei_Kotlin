e7a7e09cd3dbdecd934a78cad28957c00dc9221d
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-01-24 11:39:42 +0300

    loops && ranges

--- src/main/kotlin/R_4_02_lambda_functions.kt
+++ src/main/kotlin/R_4_02_lambda_functions.kt
@@ -1,3 +1,11 @@
+/* return from function */
+fun toSeconds(time: String): (Int) -> Int = when (time) {
+    "hour" -> { value -> value * 60 * 60 }
+    "minute" -> { value -> value * 60 }
+    "second" -> { value -> value }
+    else -> { value -> value }
+}
+
 fun main() {
     val upperCase1: (String) -> String = { str: String -> str.toUpperCase() }
     val upperCase2: (String) -> String = { str -> str.toUpperCase() }
@@ -12,4 +20,10 @@ fun main() {
 //    println(upperCase4("sensei"))
     println(upperCase5("sensei"))
     println(upperCase6("sensei"))
+    println({ string: String -> string.capitalize() }("ricky sensei"))
+
+    val timeInMinutes = listOf(2,10,15,1)
+    val min2sec = toSeconds("minutes")
+    val totalTimeInSeconds = timeInMinutes.map(min2sec).sum()
+    println("Total time is $totalTimeInSeconds secs")
 }
