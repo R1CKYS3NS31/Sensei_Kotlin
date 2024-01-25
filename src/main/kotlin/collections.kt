0ffab113b9cd0570acc8962e4376ca33bf1bad2d
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-01-25 09:10:45 +0300

    collection

--- src/main/kotlin/collections.kt
+++ src/main/kotlin/collections.kt
@@ -1,11 +1,11 @@
-fun main(){
-    val kitchen= listOf<String>("spoon","plate","sufuria","cooker","oven")
-    for (tool in kitchen){
+fun main() {
+    val kitchen = listOf("spoon", "plate", "sufuria", "cooker", "oven")
+    for (tool in kitchen) {
         println(tool)
     }
-    when{
-        "my oven" in kitchen-> println("let's bake")
-        else-> println("item is not in the kitchen")
+    when {
+        "my oven" in kitchen -> println("let's bake")
+        else -> println("item is not in the kitchen")
     }
     //using lambda expressions
     kitchen
@@ -13,10 +13,10 @@ fun main(){
         .sortedBy { it }
         .map { it.toUpperCase() }
         .forEach { println(it) }
-    val num=listOf<Int>(23,55,65,34,77,88,73,9,44,50)
-    num.filter { num->num>40 }
-        .forEach{ println("\t$it is>40")}
+    val num = listOf<Int>(23, 55, 65, 34, 77, 88, 73, 9, 44, 50)
+    num.filter { num -> num > 40 }
+        .forEach { println("\t$it is>40") }
     //or
-    num.filter { it<50 }
+    num.filter { it < 50 }
         .forEach { println("\t#$it is <50") }
 }
