426787becfcd2e06c5e913695f772aa3f31a8605
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-02-05 10:01:11 +0300

    labels - local && non-local return

--- src/main/kotlin/R_1_02_functions.kt
+++ src/main/kotlin/R_1_02_functions.kt
@@ -18,4 +18,32 @@ fun main() {
     printMessageWithPrefix(prefix = "Sensei", message = "I got you...")
     println("the sum is ${add(56, 32)}")
     println("The multiplication is ${multiply(32, 65)}")
+
+    nonLocalReturn()
+    localReturnWithLabel()
+}
+
+fun nonLocalReturn() {
+//    listOf(1, 2, 3, 4, 5).forEach {
+//        if (it == 3) return // non-local return directly to the caller of nonLocalReturn()
+//        println(it)
+//    }
+//    println("this point is unreachable")
+    run loop@{
+        listOf(1,2,3,4,5).forEach{
+            if (it ==3) return@loop // non-local return from the lambda passed to run
+            println(it)
+        }
+    }
+    println("done with nested loop with non-local return")
+}
+
+fun localReturnWithLabel() {
+//    listOf(1, 2, 3, 4, 5).forEach {
+//        if (it == 3) return@forEach // or with label
+    listOf(1, 2, 3, 4, 5).forEach lit@{
+        if (it == 3) return@lit // local return to the caller of the lambda - the forEach loop
+        println(it)
+    }
+    println("done with explicit label with local return")
 }
