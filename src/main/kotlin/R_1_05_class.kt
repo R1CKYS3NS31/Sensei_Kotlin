2ca776e1c8a811addfa2494ff6a1f85240d67562
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-02-05 11:04:18 +0300

    class inheritance

--- src/main/kotlin/R_1_05_class.kt
+++ src/main/kotlin/R_1_05_class.kt
@@ -10,4 +10,40 @@ fun main() {
     println("Default: ${contact.email}")
     contact.email = "Sensei@gmail.com"//updates property email of instance contact
     println("Updated: ${contact.email}")//calling updated parameter email of instance contact
+
+    val fr = FilledRectangle()
+    fr.draw()
 }
+
+open class Rectangle_ {
+    open fun draw() {
+        println("Drawing a rectangle")
+    }
+
+    val borderColor: String
+        get() = "black"
+}
+
+class FilledRectangle : Rectangle_() {
+    override fun draw() {
+//        super.draw()
+        val filer = Filler()
+        println("Filling the rectangle")
+    }
+
+    inner class Filler {
+        fun fill() {
+            println("Filling")
+        }
+
+        fun drawAndFill() {
+            super@FilledRectangle.draw()
+            fill()
+            println("Draw a filled rectangle with color ${super@FilledRectangle.borderColor}")
+        }
+    }
+
+    val fillColor: String
+        get() = super.borderColor
+}
+
--- src/main/kotlin/R_3_03_sealed_class.kt
+++ src/main/kotlin/R_3_03_sealed_class.kt
@@ -1,11 +1,11 @@
 sealed class Mammal(val name: String)
 // sealed class let you restrict the use of inheritance
-class Cat(val catName: String) : Mammal(catName)
-class Human_(val humanName: String, val job: String) : Mammal(humanName)
+class Cat(private val catName: String) : Mammal(catName)
+class Human_(private val humanName: String, val job: String) : Mammal(humanName)
 fun greetMammal(mammal: Mammal):String{
-    when(mammal){
-        is Human_ -> return "Hello ${mammal.name}; You're working as a ${mammal.job}"
-        is Cat -> return "Hello ${mammal.name}"
+    return when(mammal){
+        is Human_ -> "Hello ${mammal.name}; You're working as a ${mammal.job}"
+        is Cat -> "Hello ${mammal.name}"
     }
 }
 fun main(){
