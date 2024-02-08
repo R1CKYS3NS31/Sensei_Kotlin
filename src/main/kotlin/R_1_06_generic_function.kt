1a58b957eeb4ae869646a90fd33a7d8693f774a3
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-02-08 15:05:16 +0300

    generic constraints

--- src/main/kotlin/R_1_06_generic_function.kt
+++ src/main/kotlin/R_1_06_generic_function.kt
@@ -1,6 +1,16 @@
 fun <E> mutableDecimals(vararg elements: E) = mutableSetOf(*elements) //E is any-letter generic type parameter
+
+/* generic constraints */
+fun <T : Comparable<T>> sort(list: List<T>) {
+    return println(list)
+}
+
 fun main() {
     val stack = mutableDecimals(0.45, 45.34, 5.3) //inferred 'any-type' generic parameter
     println("generic type decimal parameter: $stack")
+
+    val colors = listOf("Red", "Blue", "Green")
+    sort(colors)
+//    sort(listOf(HashMap<Int, String>())) // Error: HashMap<Int, String> is not a subtype of Comparable<HashMap<Int, String>>
 }
 
